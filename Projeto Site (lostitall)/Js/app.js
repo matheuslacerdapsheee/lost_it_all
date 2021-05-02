a29c9cac70b31df5aa4bf253f7187e97a7c1801e
matheuslacerda_pshee <matheuslacerda692@gmail.com>
2021-05-02 20:00:10 -0300

    Site gerado
    incluí os arquivos do projeto que foi realizado anteriormente em pastas locais

---
+++ Projeto Site (lostitall)/Js/app.js
@@ -0,0 +1,39 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+    duration: 10000,
+    triggerElement: intro,
+    triggerHook:0
+})
+//    .addIndicators()
+    .setPin(intro)
+    .addTo(controller);
+
+//VideoAnimation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+}, 50);  
+
+
+
+
